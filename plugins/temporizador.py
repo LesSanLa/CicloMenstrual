36891baf15d7c68b7abc42720121847b40dc49a1
LesSanLa <lesly.mayte488@comunidad.unam.mx>
2021-06-13 19:46:09 -0500

    Temporizador a agregar para alarma

---
+++ plugins/temporizador.py
@@ -0,0 +1,12 @@
+import time
+
+print("ingresa un tiempo:")
+var = int(input())
+#si el tiempo es en horas(para la alarma):
+#var=var*3600
+later = time.time() + var
+later1 =  time.ctime(later)
+tiempo = time.ctime()
+while (tiempo < later1):
+    tiempo = time.ctime()
+print("ALARMA")
