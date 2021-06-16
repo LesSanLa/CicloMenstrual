c4ef6c855da51ad3bc05d5c8c7fff45be3104555
LesSanLa <lesly.mayte488@comunidad.unam.mx>
2021-06-16 18:36:44 -0500

    Contadores de alarmas en un solo .py

--- plugins/horas_metodos.py
+++ plugins/horas_metodos.py
@@ -31,20 +31,94 @@ def c_Perdidas (*args):
     	return 'say "Deberás cambiarla en {} hrs"'.format(horas_camb)
 #-----------------------------------------------------------------------------------------------------------------
 #horas para la toalla ecologica
-def toallaE(*args):
+def E_Abundante(*args):
     horas_us = int(args[0])
-    horas_camb = 4 - horas_us 
+    horas_camb = 4 - horas_us
     if (horas_camb <= 0):
-        return 'say "Cambia tu toalla lo antes posible, al ser ecologica no hay tanto riesgo de infección"'
-    if (horas_camb > 0):
-        return 'say "En aproximadamente {} hrs, deberás cambiarla"'.format(horas_camb)
-#-----------------------------------------------------------------------------------------------------------------
-#horas para tampon
-def tampon(*args):
+        return 'say "Estás en el límite, deberas cambiarla lo antes posible para evirar derrames"'
+    if (horas_camb == 2):
+        return 'say "Recuerda checar tu toalla si te sientes muy humeda para evitar accidentes"'
+    if (horas_camb >0):
+        return 'say "En aproximadamente {} horas, deberás cambiarla"'.format(horas_camb)
+
+def E_Moderado(*args):
     horas_us = int(args[0])
-    horas_camb = 8 - horas_us
-    if (horas_camb <= 0):
+    horas_camb1 = 2 - horas_us
+    horas_cam2 = 4 - horas_us
+    if (horas_cam2 <=0):
+        return 'say "Debes de cambiarla lo antes posible, no es recomendable estar más de 4 horas"'
+    if (horas_cam2 > 0 and horas_camb1 <=0):
+        return 'say "Podrías cambiarla para evitar derrames."'
+    if (horas_camb1>0):
+        return 'say "Puede cambiarlo en {} hrs o {} hrs (sin pasar las 4 hrs)"'.format(horas_cam2,horas_camb1)
+
+def E_Perdidas (*args):
+    horas_us = int(args[0])
+    horas_camb = 4 - horas_us
+    if (horas_camb <=0):
         return 'say "Debes cambiarla lo antes posible, para evitar infecciones"'
     if (horas_camb > 0):
-        return 'say "En aproximadamente {} hrs, deberás cambiarlo"'.format(horas_camb)
+        return 'say "Deberás cambiarla en {} hrs"'.format(horas_camb)
+#---------------------------------------------------------------------------------------------------------------------------
+#Toalla desechable.
+def D_Abundante(*args):
+    horas_us = int(args[0])
+    horas_camb = 4 - horas_us
+    if (horas_camb <= 0):
+        return 'say "Estás en el límite, deberas cambiarla lo antes posible para evirar derrames"'
+    if (horas_camb == 2):
+        return 'say "Recuerda checar tu toalla si te sientes muy humeda para evitar accidentes"'
+    if (horas_camb >0):
+        return 'say "En aproximadamente {} horas, deberás cambiarla"'.format(horas_camb)
+
+def D_Moderado(*args):
+    horas_us = int(args[0])
+    horas_camb1 = 2 - horas_us
+    horas_cam2 = 4 - horas_us
+    if (horas_cam2 <=0):
+        return 'say "Debes de cambiarla lo antes posible, no es recomendable estar más de 4 horas"'
+    if (horas_cam2 > 0 and horas_camb1 <=0):
+        return 'say "Podrías cambiarla para evitar derrames."'
+    if (horas_camb1>0):
+        return 'say "Puede cambiarlo en {} hrs o {} hrs (sin pasar las 4 hrs)"'.format(horas_cam2,horas_camb1)
+
+def D_Perdidas (*args):
+    horas_us = int(args[0])
+    horas_camb = 4 - horas_us
+    if (horas_camb <=0):
+        return 'say "Debes cambiarla lo antes posible, para evitar infecciones"'
+    if (horas_camb > 0):
+        return 'say "Deberás cambiarla en {} hrs"'.format(horas_camb)
+
+#-------------------------------------------------------------------------------------------------------------------------
+#Tampon
+def tam_Abundante(*args):
+    horas_us = int(args[0])
+    horas_camb = 4 - horas_us 
+    if (horas_camb <= 0): 
+        return 'say "Estás en el límite, deberas cambiarlo lo antes posible para evirar derrames"'
+    if (horas_camb == 2): 
+        return 'say "¿Cómo te sientes?. Podrías cambiar tu tampon ahora o esperar un poco más."'
+    if (horas_camb >0):
+        return 'say "En aproximadamente {} horas, deberás cambiarla"'.format(horas_camb)
+
+def tam_Moderado(*args):
+    horas_us = int(args[0])
+    horas_camb1 = 6 - horas_us
+    horas_cam2 = 8 - horas_us
+    if (horas_cam2 <=0):
+        return 'say "Debes de cambiarlo lo antes posible, no es recomendable traer un tampon por más de 8 horas"'
+    if (horas_cam2 > 0 and horas_camb1 <=0):
+        return 'say "Podrías cambiarlo para evitar derrames o esperar un poco más"'
+    if (horas_camb1>0):
+        return 'say "Puede cambiarlo en {} hrs o {} hrs (sin pasar las 8 hrs)"'.format(horas_cam2,horas_camb1)
+
+def tam_Perdidas (*args):
+    horas_us = int(args[0])
+    horas_camb = 8 - horas_us
+    if (horas_camb <=0):
+        return 'say "Debes cambiarla lo antes posible, para evitar infecciones"'
+    if (horas_camb > 0): 
+        return 'say "Deberás cambiarla en {} hrs"'.format(horas_camb)
+
 
--- plugins/temporizador.py
+++ plugins/temporizador.py
@@ -8,5 +8,6 @@ later = time.time() + var
 later1 =  time.ctime(later)
 tiempo = time.ctime()
 while (tiempo < later1):
-    tiempo = time.ctime()
+   tiempo = time.ctime()
+#print ("hora:",later1)
 print("ALARMA")
